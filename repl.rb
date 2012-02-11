e282b0a2940f3cba855f71b9a1c1224631fb263e
Jackson <railsmail42@gmail.com>
2012-02-10 19:55:34 -0600

    Added Readline and error handling to REPL

--- repl.rb
+++ repl.rb
@@ -1,15 +1,20 @@
 # by Russ Olsen
 # http://eloquentruby.com
 
+require 'readline'
 require 'lithp'
 require 'reader'
 
 lisp = Lisp.new
 
-print "> "
-while not $stdin.eof?
-  line = readline
-  s_expression = SExpressionParser.new(line).parse
-  p lisp.eval(s_expression)
-  print "> "
+loop do
+  line = Readline.readline("> ", true)
+  exit if line =~ /^exit$/i
+  begin
+    s_expression = SExpressionParser.new(line).parse
+    p lisp.eval(s_expression)
+  rescue Exception => error
+    # ANSI escaped red
+    puts "\e[31m#{error}\e[0m"
+  end
 end
